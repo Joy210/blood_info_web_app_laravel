4b6d9ba7fb6f0bd244d60c2d3095b19a47a8d957
joy210 <sohanoor404@gmail.com>
2020-02-20 18:14:15 +0600

    users.sql

---
+++ users.sql
@@ -0,0 +1,26 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.11-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `users` (
+	`@÷³` bigint (20),
+	`x÷³` varchar (765),
+	`°÷³` varchar (765),
+	`è÷³` varchar (765),
+	`0ø³` varchar (765),
+	`xø³` varchar (765),
+	`°ø³` varchar (765),
+	`øø³` varchar (765),
+	`@ù³` varchar (765),
+	`xù³` varchar (765),
+	`Àù³` varchar (300),
+	`ú³` timestamp ,
+	`Pú³` timestamp 
+); 
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('3','Sohanur Rahman','hello@gmail.com','01720000048','AB+','1044542545.png','3','19','155','$2y$10$ZGJ5Hvpr.yMcDtBlEz56Du4sPO0MAZULTE.oLQkW6skvRqd7WDPnC',NULL,'2020-02-13 10:13:24','2020-02-13 11:46:46');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('4','S. Rahman','hello1@gmail.com','01720000048','AB+','898120390.jpg','1','2','8','$2y$10$z0kmTU1jSoBNUhvC/KwtEuI/XXPMbnTkLmbgzORRtnfUKfT9cMtj.',NULL,'2020-02-20 11:32:06','2020-02-20 11:32:38');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('5','Shahida Begum','shahida@gmail.com','01719141703','A+','990862007.jpg','2','8','51','$2y$10$xU0hcVKIjt9Vfkgk5j4x9e0m9RqsrX0QWYyoiQnna/UCfYlw6IOua',NULL,'2020-02-20 11:34:57','2020-02-20 11:35:44');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('6','Hasib','hasib@gmail.com','01813082175','B+','637051944.jpg','1','2','8','$2y$10$7vKTyrCB2lYeDOTXeIkVhe5pkIBlgMMaxJul.lLyTivm61NrKlzxS',NULL,'2020-02-20 11:48:34','2020-02-20 11:48:59');
