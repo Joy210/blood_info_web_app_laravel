01b2a6d29799e9c45b706b2ff68fbb616ea41bc0
joy210 <md.sohanur.rahman.joy@gmail.com>
2020-02-22 05:15:54 +0600

    updaer user.sql

--- users.sql
+++ users.sql
@@ -6,21 +6,21 @@ MySQL - 5.5.5-10.4.11-MariaDB
 /*!40101 SET NAMES utf8 */;
 
 create table `users` (
-	`@÷³` bigint (20),
-	`x÷³` varchar (765),
-	`°÷³` varchar (765),
-	`è÷³` varchar (765),
-	`0ø³` varchar (765),
-	`xø³` varchar (765),
-	`°ø³` varchar (765),
-	`øø³` varchar (765),
-	`@ù³` varchar (765),
-	`xù³` varchar (765),
-	`Àù³` varchar (300),
-	`ú³` timestamp ,
-	`Pú³` timestamp 
+	`àf•` bigint (20),
+	`g•` varchar (765),
+	`Pg•` varchar (765),
+	`ˆg•` varchar (765),
+	`Ðg•` varchar (765),
+	`h•` varchar (765),
+	`Ph•` varchar (765),
+	`˜h•` varchar (765),
+	`àh•` varchar (765),
+	`i•` varchar (765),
+	``i•` varchar (300),
+	`¨i•` timestamp ,
+	`ði•` timestamp 
 ); 
-insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('3','Sohanur Rahman','hello@gmail.com','01720000048','AB+','1044542545.png','3','19','155','$2y$10$ZGJ5Hvpr.yMcDtBlEz56Du4sPO0MAZULTE.oLQkW6skvRqd7WDPnC',NULL,'2020-02-13 10:13:24','2020-02-13 11:46:46');
-insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('4','S. Rahman','hello1@gmail.com','01720000048','AB+','898120390.jpg','1','2','8','$2y$10$z0kmTU1jSoBNUhvC/KwtEuI/XXPMbnTkLmbgzORRtnfUKfT9cMtj.',NULL,'2020-02-20 11:32:06','2020-02-20 11:32:38');
-insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('5','Shahida Begum','shahida@gmail.com','01719141703','A+','990862007.jpg','2','8','51','$2y$10$xU0hcVKIjt9Vfkgk5j4x9e0m9RqsrX0QWYyoiQnna/UCfYlw6IOua',NULL,'2020-02-20 11:34:57','2020-02-20 11:35:44');
-insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('6','Hasib','hasib@gmail.com','01813082175','B+','637051944.jpg','1','2','8','$2y$10$7vKTyrCB2lYeDOTXeIkVhe5pkIBlgMMaxJul.lLyTivm61NrKlzxS',NULL,'2020-02-20 11:48:34','2020-02-20 11:48:59');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('1','S. Rahman','sohanoor404@gmail.com','01922189655','A+','1639275466.jpg','3','34','269','$2y$10$0sW7EdBhoXuslVyGVXdtge6rt3Uz3h.5UeEzHHQeMQjgvP4heuDnC',NULL,'2020-02-13 16:20:44','2020-02-13 16:21:19');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('2','Sohanur Rahman','sohanur1@gmail.com','2','B+','1696078504.jpg','1','6','39','$2y$10$lHiq75rwWmhLw799/Sy8l.LGpNTvZwM6Xk839Xz2asK48.9vRRTZ6',NULL,'2020-02-13 17:52:15','2020-02-13 17:52:50');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('3','Hasib','hasib@gmail.com','3','B+','255023529.png','5','50','374','$2y$10$g6lwj8XKHQWSjcIhhVW2N.bTjL4lYsN/tkKVpKIH4JL7EliIkJpZS',NULL,'2020-02-13 19:57:20','2020-02-13 19:58:09');
+insert into `users` (`id`, `name`, `email`, `mobile_no`, `blood_group`, `image`, `division`, `district`, `upazila`, `password`, `remember_token`, `created_at`, `updated_at`) values('4','Siyam','siyam@gmail.com','4','AB+','629760934.jpg','2','8','51','$2y$10$0ylwbtuJ4teWo3MpvkgWz.4t5IqhhpwBLtg/nWxGxPCooBgUrhWqG',NULL,'2020-02-21 17:52:03','2020-02-21 18:26:16');
